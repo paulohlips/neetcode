697b6e5764c7b3eb864f7d4ace1d56e2b2d788c5
paulohlips <paulohfcmendes@gmail.com>
2024-07-16 22:53:16 -0300

    ex03: fixes if statement

--- 01-arrays-and-hashing/03-two-sum.js
+++ 01-arrays-and-hashing/03-two-sum.js
@@ -5,8 +5,8 @@
  */
 function twoSumQuadratic(nums, target) {
   for (let i = 0; i < nums.length; i++) {
-    for (let j = 0; j < nums.length; j++) {
-      if (nums[i] !== nums[j] && nums[i] + nums[j] === target) {
+    for (let j = 1; j <= nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
         return [i, j];
       }
     }
