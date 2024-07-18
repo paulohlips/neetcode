34b9094d4905d9685395bb745276930309f172b6
paulohlips <paulohfcmendes@gmail.com>
2024-07-18 06:38:41 -0300

    ex05: top key frequent elements

---
+++ 01-arrays-and-hashing/05-top-k-frquent-elements.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+const topKFrequent = (nums, k) => {
+  let map = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (!map[nums[i]]) {
+      map[nums[i]] = 1;
+      continue;
+    }
+
+    map[nums[i]] = map[nums[i]] + 1;
+  }
+
+  const values = Object.entries(map).sort();
+  console.log(values);
+  const result = [];
+  for (let j = 0; j < k; j++) {
+    result.push(values[j][0]);
+  }
+  return result;
+};
+
+console.log(topKFrequent([4, 1, -1, 2, -1, 2, 3], 2));
+console.log(topKFrequent([1, 1, 1, 2, 2, 3], 2));
+console.log(topKFrequent([1], 1));
